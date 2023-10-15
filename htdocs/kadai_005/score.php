b8f379706fd0d44436c1dc65e49403f05264157f
myuser3 <138294906+myuser3@users.noreply.github.com>
2023-10-15 18:06:52 +0900

    Create score.php

---
+++ htdocs/kadai_005/score.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>PHPで平均点を求める</title>
+</head>
+<body>
+  <?php
+  $sum = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+  $score = $sum/10;
+  echo $score
+  ?>
+</body>
+</html>
