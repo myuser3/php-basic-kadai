bc5bd92bc95c442d919e5e0822db51559e009df3
myuser3 <138294906+myuser3@users.noreply.github.com>
2023-10-18 20:17:28 +0900

    Update foreach.php

--- htdocs/kadai_011/foreach.php
+++ htdocs/kadai_011/foreach.php
@@ -1,3 +1,9 @@
 <?php
+$vegetable = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+
+// 連想配列$vegetableのキーと値を1つずつ出させる
+foreach ($vegetable as $each => $value) {
+  echo "{$each} : {$value}<br>"; 
+}
 
 ?>
